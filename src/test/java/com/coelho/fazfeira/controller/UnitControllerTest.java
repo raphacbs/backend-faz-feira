7ddebc13ae7f226379c6d04e4abcb3dc38cbfbcd
Raphael Coelho <raphaelcoel@gmail.com>
2023-11-04 15:28:28 -0300

    remove test

--- src/test/java/com/coelho/fazfeira/controller/UnitControllerTest.java
+++ src/test/java/com/coelho/fazfeira/controller/UnitControllerTest.java
@@ -5,6 +5,7 @@ import com.coelho.fazfeira.builder.UnitRequestBodyBuilder;
 import com.coelho.fazfeira.dto.UnitRequestBody;
 import com.coelho.fazfeira.service.UnitService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
@@ -45,15 +46,16 @@ class UnitControllerTest {
         when(unitService.create(any(UnitRequestBody.class))).thenReturn(UnitDtoBuilder.create(unitRequestBody));
     }
 
-    @Test
-    void whenPostRequestToUnitsAndValidUnit_then201() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/units")
-                        .content(json.write(unitRequestBody).getJson())
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isCreated())
-                .andDo(print())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
-    }
+//    @Test
+//
+//    void whenPostRequestToUnitsAndValidUnit_then201() throws Exception {
+//        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/units")
+//                        .content(json.write(unitRequestBody).getJson())
+//                        .contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(MockMvcResultMatchers.status().isCreated())
+//                .andDo(print())
+//                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+//    }
 
 //    @Test
 //    void givenWithoutParams_whenGetRequestAll_thenReturnListUnitDto() throws Exception {
